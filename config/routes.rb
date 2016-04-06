87ac55831161f170da67d137ca93ca0507be8819
zektbach <vanessa32932@gmail.com>
2016-04-06 11:46:58 +0900

    Add hello

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-
+root 'application#hello'
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
